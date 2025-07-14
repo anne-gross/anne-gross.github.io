7586d59aeaeeac8a4eb985aa6c8f0e2018031400
Anne Gross <github@anne-gross.de>
2025-07-14 23:34:10 +0200

    correct image gallery and add object fit

--- assets/js/glider-gallery.mjs
+++ assets/js/glider-gallery.mjs
@@ -1,83 +1,88 @@
 import Glide from "@glidejs/glide";
 
 customElements.define(
-    "glider-gallery",
-    class extends HTMLElement {
-      connectedCallback() {
-        imagesLoaded(this, createGallery.bind(this));
-      }
+  "glider-gallery",
+  class extends HTMLElement {
+    connectedCallback() {
+      imagesLoaded(this, createGallery.bind(this));
     }
-  );
-  function createGallery() {
-    const lightboxId = this.getAttribute("lightbox-id");
-    this.removeAttribute("lightbox-id");
-    const images = this.querySelectorAll("img");
-    const gallery = wrapGallery(images, lightboxId);
-    this.appendChild(gallery);
-  
-    const glide = new Glide(gallery, {
-      type: "carousel",
-      perView: 4,
-      focusAt: "center",
-      breakpoints: {
-        800: {
-          perView: 2,
-        },
-        480: {
-          perView: 1,
-        },
-      },
-    });
-    glide.mount();
+  }
+);
 
-    refreshFsLightbox();
-  }
-  
-  function wrapGallery(images, lightboxId) {
-    const glide = document.createElement("div");
-    glide.classList.add("glide");
-  
-    const glideTrack = document.createElement("div");
-    glideTrack.classList.add("glide__track");
-    glideTrack.setAttribute("data-glide-el", "track");
-    glide.appendChild(glideTrack);
-  
-    const glideSlides = document.createElement("ul");
-    glideSlides.classList.add("glide__slides");
-    glideTrack.appendChild(glideSlides);
-  
-    images.forEach(img => glideSlides.appendChild(wrapImage(img, lightboxId)));
-  
-    const glideBullets = document.createElement("div");
-    glideBullets.classList.add("glide__bullets");
-    glideBullets.setAttribute("data-glide-el", "controls[nav]");
-    glide.appendChild(glideBullets);
-  
-    images.forEach((_, index) => {
-      glideBullets.appendChild(createBullet(index));
-    });
-  
-    return glide;
-  }
-  
-  function wrapImage(img, lightboxId) {
-    const slide = document.createElement("li");
-    slide.classList.add("glide__slide");
-  
-    const lightboxLink = document.createElement("a");
-    lightboxLink.setAttribute("data-fslightbox", lightboxId);
-    lightboxLink.style = "border-bottom: none";
-    lightboxLink.setAttribute("href", img.attributes["src"].value);
-    lightboxLink.appendChild(img);
-    slide.appendChild(lightboxLink);
-  
-    return slide;
-  }
-  
-  function createBullet(index) {
-    const bullet = document.createElement("button");
-    bullet.classList.add("glide__bullet");
-    bullet.setAttribute("data-fslightbox", `=${index}`);
-  
-    return bullet;
-  }
+function createGallery() {
+  const lightboxId = this.getAttribute("lightbox-id");
+  this.removeAttribute("lightbox-id");
+  const perViewMax = this.getAttribute("per-view-max") ?? 4;
+  const perViewXsmall = this.getAttribute("per-view-xsmall") ?? 2;//480
+  const perViewSmall = this.getAttribute("per-view-small") ?? 1; //768
+
+  const images = this.querySelectorAll("img");
+  const gallery = wrapGallery(images, lightboxId);
+  this.appendChild(gallery);
+
+  const glide = new Glide(gallery, {
+    type: "carousel",
+    perView: perViewMax,
+    focusAt: "center",
+    breakpoints: {
+      768: {
+        perView: perViewXsmall,
+      },
+      480: {
+        perView: perViewSmall,
+      },
+    },
+  });
+  glide.mount();
+
+  refreshFsLightbox();
+}
+
+function wrapGallery(images, lightboxId) {
+  const glide = document.createElement("div");
+  glide.classList.add("glide");
+
+  const glideTrack = document.createElement("div");
+  glideTrack.classList.add("glide__track");
+  glideTrack.setAttribute("data-glide-el", "track");
+  glide.appendChild(glideTrack);
+
+  const glideSlides = document.createElement("ul");
+  glideSlides.classList.add("glide__slides");
+  glideTrack.appendChild(glideSlides);
+
+  images.forEach(img => glideSlides.appendChild(wrapImage(img, lightboxId)));
+
+  const glideBullets = document.createElement("div");
+  glideBullets.classList.add("glide__bullets");
+  glideBullets.setAttribute("data-glide-el", "controls[nav]");
+  glide.appendChild(glideBullets);
+
+  images.forEach((_, index) => {
+    glideBullets.appendChild(createBullet(index));
+  });
+
+  return glide;
+}
+
+function wrapImage(img, lightboxId) {
+  const slide = document.createElement("li");
+  slide.classList.add("glide__slide");
+
+  const lightboxLink = document.createElement("a");
+  lightboxLink.setAttribute("data-fslightbox", lightboxId);
+  lightboxLink.style = "border-bottom: none";
+  lightboxLink.setAttribute("href", img.attributes["src"].value);
+  lightboxLink.appendChild(img);
+  slide.appendChild(lightboxLink);
+
+  return slide;
+}
+
+function createBullet(index) {
+  const bullet = document.createElement("button");
+  bullet.classList.add("glide__bullet");
+  bullet.setAttribute("data-fslightbox", `=${index}`);
+
+  return bullet;
+}
