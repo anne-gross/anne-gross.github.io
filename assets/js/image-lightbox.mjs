f50dd72f6930d767e8acc5781190e0628123aab0
Anne Gross <github@anne-gross.de>
2025-07-31 21:34:12 +0200

    switch from fslightbox to jquery-based lightbox2

--- assets/js/image-lightbox.mjs
+++ assets/js/image-lightbox.mjs
@@ -2,24 +2,25 @@ import { randomId } from "shared-kernel";
 
 customElements.define("image-lightbox", class extends HTMLElement {
     connectedCallback() {
-        imagesLoaded(this, createLightbox.bind(this));
+        document.addEventListener('DOMContentLoaded', createLightbox.bind(this));
     }
 });
 
 function createLightbox() {
     const img = this.querySelector("img");
     const lightboxId = this.getAttribute("lightbox-id") ?? randomId();
+    const title = img.getAttribute("title");
     this.removeAttribute("lightbox-id");
     
-    const lightboxLink = wrapImage(img, lightboxId);
+    const lightboxLink = wrapImage(img, lightboxId, title);
     this.appendChild(lightboxLink);
-
-    refreshFsLightbox();
 }
 
-function wrapImage(img, lightboxId) {
+function wrapImage(img, lightboxId, title) {
   const lightboxLink = document.createElement("a");
-  lightboxLink.setAttribute("data-fslightbox", lightboxId);
+  lightboxLink.setAttribute("data-lightbox", lightboxId);
+  if(title)
+    lightboxLink.setAttribute("data-title", title);
   lightboxLink.style = "border-bottom: none";
   lightboxLink.setAttribute("href", img.attributes["src"].value);
   lightboxLink.appendChild(img);
--- assets/js/masonry-gallery.mjs
+++ assets/js/masonry-gallery.mjs
@@ -1,3 +1,5 @@
+import { randomId } from "shared-kernel";
+
 customElements.define("masonry-gallery", class extends HTMLElement {
     connectedCallback() {
         imagesLoaded(this, createGallery.bind(this));
@@ -5,42 +7,45 @@ customElements.define("masonry-gallery", class extends HTMLElement {
 });
 
 function createGallery() {
+    const lightboxId = this.getAttribute("lightbox-id") ?? randomId();
+
     const pictures = this.querySelectorAll("picture");
     this.classList.add("gallery");
     this.classList.add("gallery--grid");
 
-    const galleryWrap = wrapGallery(pictures);
+    const galleryWrap = wrapGallery(pictures, lightboxId);
     this.appendChild(galleryWrap);
 
     var masonry = new Masonry(galleryWrap, {
         itemSelector: '.gallery__item',
         transitionDuration: 0
     });
-
-    refreshFsLightbox();
 }
 
-function wrapGallery(pictures) {
+function wrapGallery(pictures, lightboxId) {
     const galleryWrap = document.createElement("div");
     galleryWrap.classList.add("gallery__wrap");
-    pictures.forEach(picture => galleryWrap.appendChild(wrapPicture(picture)));
+    pictures.forEach(picture => galleryWrap.appendChild(wrapPicture(picture, lightboxId)));
 
     return galleryWrap;
 }
 
-function wrapPicture(picture) {
+function wrapPicture(picture, lightboxId) {
     const img = picture.querySelector("img");
+    const title = img.getAttribute("title");
 
     const galleryItem = document.createElement("div");
     galleryItem.classList.add("gallery__item");
 
-    const galleryLink = document.createElement("a");
-    galleryLink.classList.add("gallery__item__link");
-    galleryLink.setAttribute("data-fslightbox", "");
-    galleryLink.setAttribute("href", img.attributes["src"].value);
+    const lightboxLink = document.createElement("a");
+    lightboxLink.classList.add("gallery__item__link");
+    lightboxLink.setAttribute("data-lightbox", lightboxId);
+    if(title)
+        lightboxLink.setAttribute("data-title", title);
+    lightboxLink.setAttribute("href", img.attributes["src"].value);
 
-    galleryLink.appendChild(picture);
-    galleryItem.appendChild(galleryLink);
+    lightboxLink.appendChild(picture);
+    galleryItem.appendChild(lightboxLink);
 
     return galleryItem;
 }
